76a36200396482535e018d2eb846e135eedc9eb9
Sabbir <sabbir.ul.alam.sabbir@gmail.com>
2025-12-16 09:48:05 +0600

    init commit

---
+++ first_extension/wxt.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+     srcDir: 'src',
+     manifest: {
+          name: 'On-Page Translator',
+          description: 'Select an area, Ocr it, translate it',
+          permissions: ['activeTab','scripting','storage'],
+          host_permissions:['<all_urls>'],
+     }
+});
