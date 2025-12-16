0b80a2654560893ed0859eb492c72a4db7ad18ff
Sabbir <sabbir.ul.alam.sabbir@gmail.com>
2025-12-16 10:15:00 +0600

    Update background

--- first_extension/src/entrypoints/background.ts
+++ first_extension/src/entrypoints/background.ts
@@ -1,3 +1,20 @@
+// export default defineBackground(() => {
+//   console.log('Hello background!', { id: browser.runtime.id });
+// });
+import { browser } from 'wxt/browser';
+
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onMessage.addListener(async (msg) => {
+    if (msg.type === 'START_SELECTION') {
+      await browser.scripting.executeScript({
+        target: { tabId: msg.tabId },
+        files: ['content.js'],
+      });
+
+      browser.tabs.sendMessage(msg.tabId, {
+        type: 'ENTER_SELECTION_MODE',
+      });
+    }
+  });
 });
